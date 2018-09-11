be6a4e3b7dd6ad2d3cfc475b040c54e4401f2663
정고은 <wjdrhdms002@naver.com>
2018-09-11 15:55:03 +0900

    Update contract address

--- ethereum/metaGalaga.js
+++ ethereum/metaGalaga.js
@@ -2,6 +2,7 @@ import web3 from './web3';
 import MetaGalaga from './build/MetaGalaga.json';
 const instance = new web3.eth.Contract(
     JSON.parse(MetaGalaga.interface),
+    '0x8101487270f5411cf213b8d348a2ab46df66245d'
 );
 
 export default instance;
